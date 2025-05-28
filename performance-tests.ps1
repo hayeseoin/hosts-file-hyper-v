826590d1299acfe663de238ea0a5bee42c8b2a36
Eoin Hayes <eoin.hayes@terminalfour.com>
2025-05-28 09:25:39 +0200

    starting interface check

--- performance-tests.ps1
+++ performance-tests.ps1
@@ -1,9 +1,5 @@
-echo 'Get VM list'
-Measure-Command { Get-VM | Where-Object { $_.State -eq "Running" } }
+echo 'These are some performance checks..'
 
-$name = "eight-four.local"
-echo 'Get IPs'
-Measure-Command { (Get-VMNetworkAdapter -VMName $name).IPAddresses | Where-Object { $_ -match '^[0-256].*\.*\.*' } }
-
-echo 'Get Uptime'
-Measure-Command { (Get-VMNetworkAdapter -VMName $name).Uptime | Where-Object { $_ -match '^[0-256].*\.*\.*' } }
+$timeTaken = Measure-Command { .\get-interfaces.ps1 }
+echo 'in milliseconds'
+echo $timeTaken.TotalMilliseconds
