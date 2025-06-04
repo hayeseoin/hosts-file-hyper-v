3a3ebaaeb4b5914c9df5464032fe4dcffe2430c2
Eoin Hayes <eoahayes13@gmail.com>
2025-06-04 13:29:10 +0200

    fix bug and enhanced logging
    the update hosts file checks the hosts file for the presence of the VM name from the cache file, but there is a typo in the if stateement (that has been fixed) which caused it to look for all(?) the names from the file. Seems to have helped the issue where the hosts file gets deleted

--- update_hosts.ps1
+++ update_hosts.ps1
@@ -5,10 +5,12 @@ $cachedVMFile = Get-Content $config.hypervHostsCache | ConvertFrom-Json
 $hostsFile = Get-Content $config.hosts_file_path
 $updatedHostsFile = $hostsFile.Clone()
 
-# if ($hostsFile.Count -lt 2) {
-#     Write-Log "WARNING: Hosts file appears to be empty or malformed. Aborting to avoid overwrite."
-#     exit 1
-# }
+if ($config.debug_flag) {
+    Write-Log "DEBUG --- Hosts File Clone START ---"
+    Write-Log "$updatedHostsFile"
+    Write-Log "DEBUG --- Hosts File Clone END ---"
+}
+
 
 foreach ($hypervHost in $cachedVMFile.virtual_machines) {
     $name = $hypervHost.Name
@@ -26,19 +28,38 @@ foreach ($hypervHost in $cachedVMFile.virtual_machines) {
     # Match Power Toys Hosts File GUI editor
     $hostEntry = "  " + $ips.PadRight(15) + $name
 
-    if (-not ($hypervHost -like "*.local")) {
+    if ($config.debug_flag) {
+    Write-Log "DEBUG --- Host entry for $name START ---"
+    Write-Log "$hostEntry"
+    Write-Log "DEBUG --- Host entry for $name END ---"
+        }
+
+    if (-not ($name -like "*.local")) {
         $hostEntry += " $name.local"
         Write-Log "Adding $name.local as well."
     }
     if ($index) {
         $lineIndex = ($index - 1)
+            if ($config.debug_flag) {
+                Write-Log "DEBUG --- Line indexes for $name START ---"
+                Write-Log "Index: $index"
+                Write-Log "Line Index: $lineIndex"
+                Write-Log "DEBUG --- Line indexes for $name END ---"
+            }
         Write-Log "Replacing host entry for $name"
         $updatedHostsFile[$lineIndex] = $hostEntry
     } else {
         Write-Log "Adding host entry for $name"
         $updatedHostsFile += $hostEntry
     }
+
+    if ($config.debug_flag) {
+    Write-Log "DEBUG --- Hosts at end of loop for $name START ---"
+    Write-Log "$updatedHostsFile"
+    Write-Log "DEBUG --- Hosts at end of loop for $name $name END ---"
+    }
 }
+
 
 $updatedHostsFileLength = $updatedHostsFile.Count
 Write-Log "Updating hosts file."
